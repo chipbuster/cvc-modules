aea19b427b09589de4a78ce190830e2d09f62db5
Kevin Song <kevin.s05@gmail.com>
2017-08-10 19:54:42 -0500

    Add sl and parallel to modules

---
+++ ICES/parallel/20170722.lua
@@ -0,0 +1,10 @@
+help([[
+  GNU Parallel, for all of your bash-level parallelism needs
+]])
+whatis("Version: 20170722")
+whatis("Keywords: parallel, cvc")
+whatis("URL: https://www.gnu.org/software/parallel/")
+whatis("Description: A shell tool for executing jobs in parallel using one or more computers")
+
+prepend_path( "PATH",           "/org/centers/cvc/software/c7/programs/parallel/20170722/bin")
+prepend_path( "MANPATH",           "/org/centers/cvc/software/c7/programs/parallel/20170722/share/man")
---
+++ ICES/sl/latest.lua
@@ -0,0 +1,11 @@
+help([[
+  It's a Steam locomotive.
+  See http://store.steampowered.com/ for details.
+]])
+whatis("Version: only god knows")
+whatis("Keywords: trains, yousuckattyping, cvc")
+whatis("URL: https://www.youtube.com/watch?v=BxrF9Goaei8")
+whatis("Description: A replacement so that whenever you mistype ls you get hit by a train.")
+
+prepend_path( "PATH",   "/org/centers/cvc/software/c7/programs/sl/bin")
+prepend_path( "MANPATH","/org/centers/cvc/software/c7/programs/sl/share/man")
